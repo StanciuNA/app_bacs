04a8e1913d6d6e0c09bdc72738c97c3cddc9ce91
NicuTest <nst@evergrowth.io>
2023-07-23 18:55:37 +0300

    Setting up the skeleton of the project

---
+++ bacsApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BacsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bacsApp'
---
+++ bacsApp/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class Utilisateur(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+class Bac(models.Model):
+    libelle = models.CharField(max_length=30)
+
+class Site(models.Model):
+    libelle = models.CharField(max_length=30)
+    furnisseur = models.BooleanField(max_length=30)
+    bac = models.ManyToManyField(Bac,through="Stock")
+
+class Documnet(models.Model):
+    url = models.CharField(max_length=255,null=True)
+
+class Livraison(models.Model):
+    site = models.ForeignKey(Site, on_delete = models.SET_NULL, null= True)
+    date = models.CharField(max_length=30)
+    retour = models.CharField(max_length=30)
+    utilisateur = models.ForeignKey(Utilisateur, on_delete = models.SET_NULL, null= True)
+    livraison = models.ManyToManyField(Bac,through="Cargaison")
+    document = models.ManyToManyField(Documnet)
+
+
+class Cargaison(models.Model):
+    bac = models.ForeignKey(Bac,on_delete=models.SET_NULL, null=True)
+    livraison = models.ForeignKey(Livraison,on_delete=models.SET_NULL, null=True)
+    quantite = models.PositiveIntegerField()
+    
+
+class Stock(models.Model):
+    quantite = models.PositiveIntegerField()
+    site = models.ForeignKey(Site,on_delete=models.SET_NULL, null=True)
+    bac = models.ForeignKey(Bac,on_delete=models.SET_NULL, null=True)
+
+
+    
